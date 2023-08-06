bb785c715ab8b4738cfd63cd67249f8f73bdf5f7
Henry Leão <henrylsilveira@gmail.com>
2023-08-05 20:20:02 -0400

    05/08/2023 - 2

--- src/server.ts
+++ src/server.ts
@@ -57,7 +57,8 @@ async function bootstrap() {
 
   await fastify.listen({
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
-    host: "3.134.238.10",
+    host: process.env.IP ? process.env.IP : "0.0.0.0"
+    // "3.134.238.10",
     //Retirar o host para executar o projeto localmente
   }).then(() => {
     console.log(`🚀 HTTP server running on ${process.env.PORT ? Number(process.env.PORT) : 3333}`)
