6183186d913c1fbeb358984a102013ab58007fa7
Henry Leão <henrylsilveira@gmail.com>
2024-05-16 11:31:02 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -67,7 +67,7 @@ async function bootstrap() {
   
   await fastify.listen({
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
-    host: '127.0.0.1',
+    host: '::',
     // ("RENDER" in process.env) ? `0.0.0.0` : `localhost`
     // "3.134.238.10",
     //Retirar o host para executar o projeto localmente
