5b50c7572ca29bd7d0cc0eacaca6489f727700c9
Henry Leão <henrylsilveira@gmail.com>
2024-07-18 15:17:31 -0300

    Update pmt.ts

--- src/routes/pmt.ts
+++ src/routes/pmt.ts
@@ -8,7 +8,7 @@ export async function pmtRoutes(fastify: FastifyInstance) {
   fastify.get('/veiculos',
     async (request, reply) => {
       const result = await prisma.viatura.findMany({
-        relationLoadStrategy: 'join',
+        relationLoadStrategy: 'query',
         select: {
           _count: true,
           CautelaViatura: true,
